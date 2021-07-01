8ee5cc37088907e3ce556f04bab50935a8d273f7
Gazo <greigazo@gmail.com>
2021-06-30 18:31:02 -0600

    patients

---
+++ 123.java
@@ -0,0 +1,18 @@
+/**
+  * NAME:
+  * DATE:
+  * FILE:
+  * COMMENTS:
+  */
+
+public class 123 extends eecs.Gui
+{
+   public static void main(String[] args)
+   {
+      // declare variables
+      ??[] ASCIITable = new ??[??];
+      
+      clear_console;
+      ASCIITable[1]	= 0;
+   } // close main
+} // close 123
